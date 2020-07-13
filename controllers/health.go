83f4a8ed16493a79acf37dbe28a1d9e5ae8869cd
Rui Fu <freelz@gmail.com>
2020-07-13 14:32:49 +0800

    health api return json result

--- controllers/health.go
+++ controllers/health.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"github.com/freeznet/tomato/types"
+)
 
 // HealthController 检测服务器健康状态
 type HealthController struct {
@@ -10,5 +13,8 @@ type HealthController struct {
 // Get 直接返回状态 200
 // @router / [get]
 func (h *HealthController) Get() {
-	h.Ctx.Output.SetStatus(200)
+	h.Data["json"] = types.M{
+		"status": "ok",
+	}
+	h.ServeJSON()
 }
